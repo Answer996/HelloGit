1e6f62d09a2a639660405c19f8846c867b2652ab
Answer <1300830481@qq.com>
2020-10-23 21:28:41 +0800

    更新1

--- HelloGit/src/main/java/git/HelloGit.java
+++ HelloGit/src/main/java/git/HelloGit.java
@@ -3,5 +3,8 @@ package git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+
     }
 }
